316538f48a83d5a3ae9651382b955407fb53fddf
Vicentemr2002 <80923963+Vicentemr2002@users.noreply.github.com>
2024-01-26 14:12:52 -0300

    OK

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,56 +11,56 @@ let thumbnailDom = document.querySelector('.card-baños .mini-fotos');
 
 
 
-openmain.onclick = () =>{
+openmain.onclick = () => {
 
     mainMenu.classList.add('menu-open');
 }
 
 
-closemain.onclick = () =>{
+closemain.onclick = () => {
 
     mainMenu.classList.remove('menu-open');
 }
 
-nextDom.onclick = function(){
+nextDom.onclick = function () {
     showSlider('next');
 }
-prevDom.onclick = function(){
-    showSlider('prev');  
+prevDom.onclick = function () {
+    showSlider('prev');
 }
 
 
 let timeRunning = 3000;
 let timeAutoNext = 7000;
 let runTimeOut;
-let runAutoRun = setTimeout(()=>{
+let runAutoRun = setTimeout(() => {
     nextDom.click();
-  }, timeAutoNext);
+}, timeAutoNext);
 
-function showSlider(type){
+function showSlider(type) {
     let itemSlider = document.querySelectorAll('.card-img-baños .card');
     let itemThumbnail = document.querySelectorAll('.card-baños .mini-fotos .card');
 
-   if(type === 'next'){
-    listItemDom.appendChild(itemSlider[0]);
-    thumbnailDom.appendChild(itemThumbnail[0]);
-    cardBañosDom.classList.add('next')
-   } else{
-    let positionLastItem = itemSlider.length -1;
-    listItemDom.prepend(itemSlider[positionLastItem])
-    thumbnailDom.prepend(itemThumbnail[positionLastItem])
-    cardBañosDom.classList.add('prev')
-   }
+    if (type === 'next') {
+        listItemDom.appendChild(itemSlider[0]);
+        thumbnailDom.appendChild(itemThumbnail[0]);
+        cardBañosDom.classList.add('next')
+    } else {
+        let positionLastItem = itemSlider.length - 1;
+        listItemDom.prepend(itemSlider[positionLastItem])
+        thumbnailDom.prepend(itemThumbnail[positionLastItem])
+        cardBañosDom.classList.add('prev')
+    }
 
-   clearTimeout(runTimeOut);   
-   runTimeOut = setTimeout(() =>{
-    cardBañosDom.classList.remove('next')
-    cardBañosDom.classList.remove('prev')
-   }, timeRunning)
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        cardBañosDom.classList.remove('next')
+        cardBañosDom.classList.remove('prev')
+    }, timeRunning)
 
-   clearTimeout(runAutoRun);
-   runAutoRun = setTimeout(()=>{
-     nextDom.click();
-   }, timeAutoNext)
+    clearTimeout(runAutoRun);
+    runAutoRun = setTimeout(() => {
+        nextDom.click();
+    }, timeAutoNext)
 }
 
