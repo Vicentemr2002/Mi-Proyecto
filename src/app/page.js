8df7fe78596a5d53b5a966c47f73dfabf1a98b44
Jose Vitriago <80923963+Vicentemr2002@users.noreply.github.com>
2024-04-23 07:52:38 -0300

    .

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 import Nav from "../componentes/nav";
 import Footer from "@/componentes/footer";
 import ContainerProductos from "@/componentes/container-productos";
-<<<<<<< HEAD
 import CardProducto from "@/componentes/card-productos";
 import Image from "next/image";
 
@@ -43,11 +42,6 @@ let Alimento = [
             "tamaño": "Adulto"
         }
     }
-=======
-import Productos from "./productos/page";
-import CardProducto from "../componentes/card-productos";
-import Image from "next/image";
->>>>>>> 7acdcb053c48fca6855dca3455de4f125f5e9922
 
 
 ]
@@ -57,42 +51,25 @@ export default function Home() {
 
         <body>
             <Nav />
-<<<<<<< HEAD
             <div className="container-baños">
                 <div className="container-img-baños">
                     <Image src="/image/container-baños.png" width={450} height={450} />
-=======
-
-            <div className="container-baños">
-                <div className="container-img-baños">
-                    <Image src="/image/container-baños.png" width={500} height={500} />
->>>>>>> 7acdcb053c48fca6855dca3455de4f125f5e9922
                 </div>
                 <div className="index-baños">
                     <div className="animal">Profesionales Al Cuidado De Du Mascotas</div>
                     <div className="adoption">Reserve Su Turno</div>
                     <div className="description-baños">Damos los turnos de lunes a viernes de 10 a 20 Hs</div>
                     <div className="buttom-baños">
-<<<<<<< HEAD
                         <button>Turnos De Baños</button>
-=======
-                    <button>Turnos De Baños</button>
->>>>>>> 7acdcb053c48fca6855dca3455de4f125f5e9922
                     </div>
                 </div>
             </div>
 
             <ContainerProductos>
 
-<<<<<<< HEAD
                 <CardProducto />
 
 
-=======
-
-                <CardProducto />
-
->>>>>>> 7acdcb053c48fca6855dca3455de4f125f5e9922
             </ContainerProductos>
 
 
