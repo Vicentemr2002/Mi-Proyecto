31aa45f3f802c348bce97e5b000e112a6a7548d2
Vicentemr2002 <80923963+Vicentemr2002@users.noreply.github.com>
2024-05-07 17:50:09 -0300

    .

--- src/app/productos/page.js
+++ src/app/productos/page.js
@@ -1,75 +1,98 @@
 import Nav from "@/componentes/nav";
 import Footer from "@/componentes/footer";
-
-export default function Productos() {
-
-    return (
-
-        <body>
-
-            <Nav />
-
-            <div className="intermedio-container-2">
-
-                <div className="section-index">
-                    <div className="title">
-                        <h3>Productos</h3>
-                    </div>
-                    <div className="index-category" id="productos">
-                        <label className="pb-2">Categoria</label>
-                        <select form="controls" className="edit-select" name="alimentos" id="filtros-categoria">
-                            <option className="select" value="todos">--Todos--</option>
+import style from './pages.css'
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import Slider from "react-slick"
 
 
-                        </select>
-                        <div className="result" id="result"></div>
-                    </div>
-                </div>
+const excellent = [
 
-                <div className="index-cards">
+    {
+        nombre: "Excellent || Gato Adulto x Fraccion",
+        alimentos_image: "/image/alimento-excellent..png",
+        precio: "1000 ARS",
+        categoria: {
+            marca: "Excellent",
+            raza: "Gato",
+            tamaño: "Adulto",
 
-                    <div className="title">
-                        <h2>Alimentos</h2>
-                    </div>
+        }
+    },
 
-                    <div className="excellent">
+]
 
-                        <div className="title">
-                            <h1>Excellent</h1>
-                            <h4>Alimento Para Perros</h4>
-                        </div>
+const settings = {
+    dots: true,
+    infinite: true,
+    autoPlay: true,
+    autoPlaySpeed: 1500,
+    speed: 500,
+    cssEase: "linear",
+    slidesToShow: 1,
+    slidesToScroll: 1,
+responsive: [
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      infinite: true,
+      dots: true
+    }
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      initialSlide: 2
+    }
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }
+]
+};
 
-                        <div className="alimento-perro">
 
-                            <div className="card-productos">
-                                <div className="img-alimento">
-                                    <img src="./img/Excelent-cachorros-razas-pequenas-fotor-bg-remover-20240307153347.png"
-                                        alt="" />
+const CardProducto = () => {
+return (    
+    <>
+        <h2 className={style.CardProductoTittles}>Los Mejores Productos</h2>
+
+
+        <div>
+            <Slider {...settings}>
+                {
+                    alimentos.map((excellent, index) => (
+                        <div key={index} className={style.cardProducto}>
+                            <div className={style.imgAlimento}>
+                                <Image src={excellent.alimentos_image} alt='Alimentos' width={300} height={300} />
+                            </div>
+                            <div className={style.contenidoAlimento}>
+                                <h3 className={style.alimento}>{excellent.nombre}</h3>
+                                <h1 className={style.precioAlimento}>{excellent.precio}</h1>
+                                <div className={style.categoria}>
+                                    <p className={style.categoriaAlimento}>{excellent.categoria.marca}</p>
+                                    <p className={style.categoriaAlimento}>{excellent.categoria.raza}</p>
+                                    <p className={style.categoriaAlimento}>{excellent.categoria.tamaño}</p>
                                 </div>
-                                <div className="contenido-alimento">
-                                    <div className="alimento">Excellent || Raza Pequeña Puppy x Fraccion</div>
-                                    <div className="precio-alimento"> 1.916ARS</div>
-                                    <div className="categoria">
-                                        <div className="categoria-alimento">Alimento Seco</div>
-                                        <div className="categoria-alimento">Excellent</div>
-                                        <div className="categoria-alimento">Alimento Para Perros</div>
-                                        <div className="categoria-alimento">Puppy</div>
-                                        <div className="categoria-alimento">Raza Pequeña</div>
-                                    </div>
-                                    <div className="button-alimento">
-                                        <button>Comprar</button>
-                                    </div>
+                                <div className={style.btn}>
+                                    <button>Comprar</button>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
+                    ))}
+            </Slider>
+        </div >
 
-            </div>
-            <Footer />
-        </body>
+    </>
+);
+};
 
-
-    );
-
-}
+export default CardProducto;
--- src/componentes/card-baños.js
+++ src/componentes/card-baños.js
@@ -11,29 +11,27 @@ const turnos = [
         raza: "Raza: Caniche",
         baños_image: "/image/Caniche.png",
         precio: {
-            Baño : "Baño: 7.500Ars",
-            Corte: "Baño y Corte: 8.900Ars"
+            baño : "Baño: 7.500Ars",
+            corte: "Baño y Corte: 8.900Ars"
         },
         duracion : "Duracion: 2 hs"
     },
     {
-        nombre: "Pro Plan || Gato Adulto x Fraccion",
-        baños_image: "/image/proplancaturinary.png",
-        precio: "1000 ARS",
-        categoria: {
-            marca: "Pro Plan",
-            raza: "Gato",
-            tamaño: "Adulto",
-        }
-    }, {
-        nombre: "Royal Canin || Perro Adulto x Fraccion",
-        alimentos_image: "/image/alimento-royal-cannin.png",
-        precio: "1000 ARS",
-        categoria: {
-            marca: "Royal Canin",
-            raza: "Perro",
-            tamaño: "Adulto",
-        }
+        raza: "Raza: Schnauzer",
+        baños_image: "/image/Shnauzer.png",
+        precio: {
+            baño : "Baño: 7.900Ars",
+            corte: "Baño y Corte:  9.500Ars"
+        },
+        duracion : "Duracion: 2 hs"
+    },    {
+        raza: "Raza: Shih Tzu ",
+        baños_image: "/image/shih tzu.png",
+        precio: {
+            baño : "Baño: 7.900Ars",
+            corte: "Baño y Corte: 9.500Ars"
+        },
+        duracion : "Duracion: 2 hs"
     },
 ];
 
@@ -88,14 +86,12 @@ const CardProducto = () => {
                         turnos.map((turno, index) => (
                             <div key={index} className={style.cardBaños}>
                                 <div className={style.imgBaños}>
-                                    <Image src={turno.baños_image} alt='Alimentos' width={400} height={400}/>
+                                    <Image src={turno.baños_image} alt='Turnos' width={400} height={400}/>
                                 </div>
                                 <div className={style.contenidoTurnos}>
-                                    <h1 className={style.raza}>{turno.raza}</h1>
-                                    <h3 className={style.precioTurnos}>
-                                        {turno.precio.Baño}
-                                        {turno.precio.Corte}
-                                    </h3>
+                                    <h1 className={style.razaTurnos}>{turno.raza}</h1>
+                                    <h3 className={style.precioTurnos}>{turno.precio.baño}</h3>
+                                    <h3 className={style.precioTurnos}>{turno.precio.corte}</h3>
                                     <div className={style.duracion}>{turno.duracion}</div>
                                     <div className={style.btn}>
                                         <button>Pedir Turno</button>
