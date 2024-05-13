1eb7caa3850a73becbf601415b0e1dc2aa989e69
Vicentemr2002 <80923963+Vicentemr2002@users.noreply.github.com>
2024-05-13 17:50:06 -0300

    .

--- src/app/productos/page.js
+++ src/app/productos/page.js
@@ -3,7 +3,6 @@ import Footer from "@/componentes/footer";
 import CardAlimetos from "./componentes/card.alimentos";
 import ContainerProductos from "@/componentes/container-productos";
 
-
 export default function producto() {
 
   return (
--- src/script/subir_producto.mjs
+++
@@ -1,6 +0,0 @@
-import app from "../../firebase";
-import producto from "./productos";
-
-for (let i = 0; i < producto.length; i++) {
-    console.log("Juan Jose es bello")
-}
